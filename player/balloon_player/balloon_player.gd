6dcd1cd163f0cde5b51512aaf88fbe4ab312d1a5
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-12-06 19:19:19 +0000

    Add shape shift to balloon player

--- player/balloon_player/balloon_player.gd
+++ player/balloon_player/balloon_player.gd
@@ -3,9 +3,13 @@ extends CharacterBody2D
 
 const BLOCK_LENGTH : float = Constants.BLOCK_LENGTH
 
+@export var to_shape : PackedScene
+
+@onready var _anim := $AnimationPlayer
+
 
 func _physics_process(_delta: float) -> void:
-	const HORIZONTAL_SPEED := 0.25 * BLOCK_LENGTH
+	const HORIZONTAL_SPEED := BLOCK_LENGTH
 	const VERTICAL_SPEED := 4.0 * BLOCK_LENGTH
 	
 	var in_dir := signf(Input.get_axis(&"move_left", &"move_right") )
@@ -13,3 +17,23 @@ func _physics_process(_delta: float) -> void:
 	velocity.y = -VERTICAL_SPEED
 
 	move_and_slide()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"shape_shift"):
+		_anim.play(&"start_shape_shift")
+
+
+func _shape_shift():
+		var parent := get_parent()
+		var next_form : PhysicsBody2D = to_shape.instantiate()
+		next_form.position = position
+		next_form.velocity = velocity
+		next_form.to_shape = load(&"res://player/balloon_player/balloon_player.tscn")
+		parent.add_child(next_form)
+		queue_free()
+
+
+func _on_hurt() -> void:
+	#TODO: implement death
+	print("DIE")
